93a58f818b0cdc21d898628be57d9a76b240142c
Tausif Islam Sheik <you@example.com>
2024-06-03 03:06:33 +0600

    Show Details button function added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText, isShowAll) =>{
+const loadPhone = async (searchText = 'iphone', isShowAll) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
     const phones = data.data;
@@ -13,9 +13,9 @@ const displayPhones = (phones, isShowAll) =>{
     phoneContainer.textContent = '';
     const showAllContainer = document.getElementById('show-all-container');
     if(phones.length > 12 && !isShowAll){
-        showAllContainer.classList.remove('hidden')
+        showAllContainer.classList.remove('hidden');
     }else{
-        showAllContainer.classList.add('hidden')
+        showAllContainer.classList.add('hidden');
     }
    if(!isShowAll){
      phones = phones.slice(0, 12);
@@ -32,7 +32,7 @@ const displayPhones = (phones, isShowAll) =>{
         <h2 class="card-title">${phone.phone_name}</h2>
         <p>${phone.slug}</p>
         <div class="card-actions">
-          <button class="btn btn-info text-white bg-blue-700 mt-4">Show Details</button>
+          <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-info text-white bg-blue-700 mt-4">Show Details</button>
         </div>
       </div>
 
@@ -44,6 +44,33 @@ const displayPhones = (phones, isShowAll) =>{
     // Hide loading spinner
 
     toggleLoadingSpinner(false)
+}
+
+const handleShowDetails = async (id) =>{
+      console.log('clicked')
+      const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+      const data = await res.json();
+      const phone = data.data;
+      showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phone) =>{
+    console.log(phone)
+    // const phoneName = document.getElementById('phone-name');
+    // phoneName.innerText = phone.name;
+    const showDetailContainer = document.getElementById('show-detail-container');
+    showDetailContainer.innerHTML = `
+    <img class="mx-auto" src="${phone.image}" alt="">
+    <h3 id="phone-name" class="font-bold text-2xl text-center my-2">${phone.name}</h3>
+    <div class="mx-10">
+         <p><span class="font-bold">Storage : </span>${phone.mainFeatures.storage}</p>
+         <p><span class="font-bold">Chip Set : </span>${phone.mainFeatures.chipSet}</p>
+         <p><span class="font-bold">Display Size : </span>${phone.mainFeatures.displaySize}</p>
+         <p><span class="font-bold">Slug : </span>${phone.slug}</p>
+         <p><span class="font-bold">Gps : </span>${phone.others?.GPS || 'No gps avaiable'}</p>
+    </div>
+    `
+    show_details_modal.showModal();
 }
 
 
