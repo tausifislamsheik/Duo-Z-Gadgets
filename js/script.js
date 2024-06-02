e0e7b09b7245e1a78ebf6ff935ca857ae86a2822
Tausif Islam Sheik <you@example.com>
2024-06-03 00:57:09 +0600

    search section added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const loadPhone = async () =>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+const loadPhone = async (searchText) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
     const phones = data.data;
     // console.log(phones)
@@ -10,10 +10,11 @@ const loadPhone = async () =>{
 const displayPhones = phones =>{
     // console.log(phones)
     const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
     phones.forEach(phone =>{
         console.log(phone)
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`
+        phoneCard.classList = `card p-4 bg-base-100 shadow-xl`
         phoneCard.innerHTML = `
         <figure class="px-10 pt-10">
         <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
@@ -30,6 +31,14 @@ const displayPhones = phones =>{
         `
         phoneContainer.appendChild(phoneCard);
     })
+}
+
+
+const handleSearch = () =>{
+    const inputField = document.getElementById('input-field');
+    const inputText = inputField.value;
+    console.log(inputText)
+    loadPhone(inputText);
 }
 
 loadPhone();
